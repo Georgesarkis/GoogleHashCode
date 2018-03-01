301faf977d9d8302037c3974da3030389a6ef5a0
Georgesarkis <sakogeorge@gmail.com>
2018-03-01 21:15:33 +0100

    counter

--- src/LengthCounter.java
+++ src/LengthCounter.java
@@ -1,9 +1,11 @@
 package hashcode;
 
 public class LengthCounter {
-	public int distence(int x, int y) {
+	public int distence(int startx, int starty, int endx , int endy) {
 		int length = 0;
-		
-		return length
+		int x = Math.abs(startx - endx);
+		int y = Math.abs(starty - endy);
+		length = x+y;
+		return length;
 	}
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	List listCars = new ArrayList();
+	List rids = new ArrayList();
+	
+}
